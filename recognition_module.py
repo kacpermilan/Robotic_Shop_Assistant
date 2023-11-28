af7b5807c6907c7281c206a03d47bcfd502445b1
kacpermilan <kacpermmilan@gmail.com>
2023-11-29 00:31:02 +0100

    chore: clean up the recognition module

--- recognition_module.py
+++ recognition_module.py
@@ -14,6 +14,39 @@ del torch
 # Apparently CUDA nie jest wykrywana jak instalujesz dlib-a przez pip-a, więc musisz to zbudować customowo:
 # https://gist.github.com/nguyenhoan1988/ed92d58054b985a1b45a521fcf8fa781
 class RecognitionModule:
+    """
+        The `RecognitionModule` class provides functionality for face recognition and barcode detection.
+
+        Args:
+            model (str): The model to use for face recognition. Default is "cnn".
+            tolerance (float): The tolerance value for face recognition. Default is 0.5.
+            frame_thickness (int): The thickness of the frame when marking faces and barcodes. Default is 2.
+            font_thickness (int): The thickness of the font when labeling faces and barcodes. Default is 2.
+
+        Attributes:
+            model (str): The model used for face recognition.
+            tolerance (float): The tolerance value for face recognition.
+            frame_thickness (int): The thickness of the frame when marking faces and barcodes.
+            font_thickness (int): The thickness of the font when labeling faces and barcodes.
+            known_faces_encodings (list): List of known faces encodings.
+            known_names (list): List of known face names.
+            known_barcodes (dict): Dictionary of known barcode data and associated information.
+
+        Methods:
+            load_known_faces(known_faces_dir, force_rebuild=False):
+                Load known faces encodings from a faces directory's cache file,
+                or create a new cache based on its contents.
+            load_known_barcodes(csv_file_path):
+                Load known barcodes from a CSV file into a dictionary.
+            create_face_encodings(known_faces_dir, cache_file):
+                Build a new face encodings, based on the faces found in the given directory,
+                and store them in the cache file.
+            detect_on_camera(video): Try to detect the known faces (and all the rest) using the
+                                     given VideoCapture instance.
+            test_on_unknown_faces(test_dir):
+                Test the currently stored known faces encodings with the images in a given test directory.
+            test_on_barcode_images(test_dir): Test the barcode detection against the images in a given test directory.
+    """
     def __init__(self, model="cnn", tolerance=0.5,
                  frame_thickness=2, font_thickness=2):
         self.model = model
@@ -99,7 +132,7 @@ class RecognitionModule:
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
-    def test_on_unknown_faces(self, test_dir: str):
+    def test_on_face_images(self, test_dir: str):
         """
         Test the currently stored known faces encodings with the images in a given test directory.
         """
@@ -140,22 +173,11 @@ class RecognitionModule:
             results = face_recognition.compare_faces(self.known_faces_encodings, face_encoding, self.tolerance)
             if True in results:
                 label = self.known_names[results.index(True)]
-                color = self.__name_to_color(label)
                 print(f"Match found: {label}")
             else:
                 label = "Generic Human"
-                color = [0, 255, 0]
 
-            darker_color = self.__darken_color(color)
-            top_left = (face_location[3], face_location[0])
-            bottom_right = (face_location[1], face_location[2])
-            cv2.rectangle(image, top_left, bottom_right, color, self.frame_thickness)
-
-            top_left = (face_location[3], face_location[2])
-            bottom_right = (face_location[1], face_location[2] + 22)
-            cv2.rectangle(image, top_left, bottom_right, darker_color, cv2.FILLED)
-            cv2.putText(image, label, (face_location[3] + 10, face_location[2] + 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), self.font_thickness)
+            self.__mark_face(image, face_location, label)
 
     def __detect_barcodes(self, image):
         color = [255, 0, 0]
@@ -180,8 +202,38 @@ class RecognitionModule:
             bottom_right = (decoded_barcode.rect.left,
                             decoded_barcode.rect.top - 22)
             cv2.rectangle(image, top_left, bottom_right, darker_color, cv2.FILLED)
-            cv2.putText(image, label,(decoded_barcode.rect.left + 10, decoded_barcode.rect.top - 6),
+            cv2.putText(image, label, (decoded_barcode.rect.left + 10, decoded_barcode.rect.top - 6),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), self.font_thickness)
+
+    def __mark_face(self, image, face_location, label):
+        """
+        :param image: The input image in which the face needs to be marked.
+        :param face_location: The coordinates of the detected face in the image.
+        :param label: The label or name associated with the detected face.
+        :return: None
+
+        Marks the detected face in the input image with a rectangle and label.
+        The face is marked with a colored rectangle and the label is displayed above the rectangle.
+
+        Example usage:
+            image = cv2.imread('input_image.jpg')
+            face_location = (x1, y1, x2, y2)  # Example face coordinates
+            label = "John"  # Example label
+            recognition_module = RecognitionModule()
+            recognition_module.__mark_face(image, face_location, label)
+        """
+        color = self.__name_to_color(label)
+        darker_color = self.__darken_color(color)
+
+        top_left = (face_location[3], face_location[0])
+        bottom_right = (face_location[1], face_location[2])
+        cv2.rectangle(image, top_left, bottom_right, color, self.frame_thickness)
+
+        top_left = (face_location[3], face_location[2])
+        bottom_right = (face_location[1], face_location[2] + 22)
+        cv2.rectangle(image, top_left, bottom_right, darker_color, cv2.FILLED)
+        cv2.putText(image, label, (face_location[3] + 10, face_location[2] + 15),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), self.font_thickness)
 
     @staticmethod
     def __name_to_color(string: str) -> list[int]:
@@ -191,6 +243,8 @@ class RecognitionModule:
         :param string: String, which first 3 letters will be used for picking colour
         :return: RGB array of a colour
         """
+        if string == "Generic Human":
+            return [0, 255, 0]
 
         return [(ord(c.lower()) - 97) * 8 for c in string[:3]]
 
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from recognition_module import RecognitionModule
 
 recognition_module = RecognitionModule(tolerance=0.575)
 
-# recognition_module.load_known_faces("known_faces")
-# recognition_module.test_on_unknown_faces("unknown_faces")
+recognition_module.load_known_faces("known_faces")
+recognition_module.test_on_face_images("unknown_faces")
 
 recognition_module.load_known_barcodes("barcodes.csv")
 recognition_module.test_on_barcode_images("barcodes")
