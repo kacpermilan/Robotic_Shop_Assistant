1e56df8053a471b5d92222faeda9daa168b81d08
kacpermilan <kacpermmilan@gmail.com>
2023-12-05 21:44:21 +0100

    feat: test the database_module and introduce unittest

--- test.py
+++ test.py
@@ -1,9 +1,33 @@
+import unittest
+import configparser
 from recognition_module import RecognitionModule
+from database_module import DatabaseModule
 
-recognition_module = RecognitionModule(tolerance=0.575)
 
-recognition_module.load_known_faces("known_faces")
-recognition_module.test_on_face_images("unknown_faces")
+class TestProductRecognition(unittest.TestCase):
+    recognition_module = None
+    database_module = None
 
-recognition_module.load_known_barcodes("barcodes.csv")
-recognition_module.test_on_barcode_images("barcodes")
+    @classmethod
+    def setUpClass(cls):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        cls.database_module = DatabaseModule("localhost", "robotic_shop_assistant",
+                                             config.get('ROBOTIC_SHOP_ASSISTANT', 'DB_USERNAME'),
+                                             config.get('ROBOTIC_SHOP_ASSISTANT', 'DB_PASSWORD'))
+
+        cls.recognition_module = RecognitionModule(tolerance=0.575)
+        cls.recognition_module.set_product_data_source(cls.database_module)
+
+    def test_known_faces(self):
+        self.recognition_module.load_known_faces("known_faces")
+        self.recognition_module.test_on_face_images("unknown_faces")
+
+    def test_known_barcodes(self):
+        self.database_module.refresh_data()
+        self.recognition_module.test_on_barcode_images("barcodes")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
